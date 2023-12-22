b67a83de8eff87afd3c87ab2cfb6af1e1f791107
Magno Souza <127249306+MSouza27@users.noreply.github.com>
2023-12-22 01:54:33 -0300

    Add files via upload

---
+++ trilha-net-poo-desafio/Models/Nokia.cs
@@ -0,0 +1,21 @@
+
+using System.Security.Cryptography.X509Certificates;
+
+namespace DesafioPOO.Models
+{
+    public class Nokia : Smartphone
+    {
+
+        public Nokia(string numero, string modelo, string imei, int memoria)
+        : base(numero, modelo, imei, memoria)
+        {
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+
+            Console.WriteLine(nomeApp);
+        }
+
+
+    }
+}
---
+++ trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
+
+        Console.WriteLine("Smartphone Nokia");
+        Smartphone nokia = new Nokia(numero: "9999", modelo: "3310", imei: "123456789", memoria: 128);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Facebook");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone Iphone");
+        Smartphone iphone = new Iphone(numero: "9999", modelo: "3310", imei: "123456789", memoria: 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Google");
+
